d18a68b9db0b107e55f28146694ac0a81f6a67e2
Anon <73074342+Anon545@users.noreply.github.com>
2021-06-05 05:41:19 +0500

    Add files via upload

---
+++ логика простая строки1.py
@@ -0,0 +1,39 @@
+s1 = 'This Book Attemps'
+s2 = '345678'
+s3 = '345.567'
+s4 = 'extrai n8th'
+# print (s1)
+
+# f = 'book'
+# tr = f in s1
+# print (tr)
+
+# a = s1.index('Attemps')
+# print(a)
+# b = s1.rindex('s')
+# c = s1.find('h')
+# d = s1.rfind('t')
+# e = s1.startswith('t')
+# f = s1.endswith ('.')
+# g = s1.count('book')
+# h = s1.replace('t', '***')
+# low = s1.lower()
+# up = s1.upper()
+# tit = s1.title()
+# g = s1.swapcase()
+# h = s4.isalnum()
+# i = s1.isalpha ()
+# j = s2.isdigit()
+# k = s4.islower()
+# l = s2.isnumeric() #состоит ли строка только из номера
+# m = s4.isupper()
+# n = s1.isspace()
+# ####################
+# o = max(s1) #символ с наибольшим числом повторений в строке s1
+# p = min(s1)
+# print(o)
+# #################
+name = 'Oleg'
+
+print ('hello, {} !'.format(name)) # подстановка имени в строку
+                                    #с нужным форматированием
---
+++ логика простая.py
@@ -0,0 +1,26 @@
+# a = int(input())
+# if a == 0:
+#     print ('true')
+# else:
+#     print ('no')
+
+a = 8
+b = 15
+print(a>b)
+print(a<b)
+print(a==b)
+print (a<=b)
+print(a>=b)
+print(a!=b)
+
+# if 5 > a > b and 10>b:
+#     print ('a>b')
+# else:
+#     print ('a<b')
+
+if a < 8:
+    print ('a<b')
+elif a == 8:
+    print ('a = 8')
+else:
+    print ('a>b')
